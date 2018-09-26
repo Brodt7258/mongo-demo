ff8173372ff610d31c4386ff312ef8c2ff3eb44f
Tim Dill <brodt7258@gmail.com>
2018-09-26 01:55:15 -0400

    subdocuments

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,16 +1,24 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
 
 before((done) => {
     mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
     mongoose.connection
-        .once('open', () => { done(); })
+        .once('open', () => { 
+            console.log('connected to Mongo');
+            done(); 
+        })
         .on('error', (err) => {
             console.warn('Error', err);
         });
 });
 
-beforeEach((done) => {
-    mongoose.connection.collections.users.drop(done);
+beforeEach(async () => {
+    try {
+        await mongoose.connection.collections.users.drop();
+    } catch(e) {
+        console.log('skip collection drop');
+    }
 });
--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
 
     let joe;
     beforeEach((done) => {
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe', likes: 0 });
         joe.save()
             .then(() => done());
     });
@@ -49,5 +49,14 @@ describe('Updating records', () => {
             User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
             done
         );
+    });
+
+    it('a user can have their likes incremented by 1', (done) => {
+        User.updateOne({ name: 'Joe' }, { $inc: { likes: 1 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user.likes === 1);
+                done();
+            });
     });
 });
